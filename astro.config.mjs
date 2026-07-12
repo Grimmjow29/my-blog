1cff668b9dcab9d5c2f27889af1f6a3bbc011594
Grimmjow29 <prabhanbugatha13@gmail.com>
2026-07-12 16:48:04 +0530

    eyy blue balooooon

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,26 +15,12 @@ export default defineConfig({
 	},
 	fonts: [
 		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
+			provider: fontProviders.google(),
+			name: 'Ubuntu',
+			cssVariable: '--font-ubuntu',
+			weights: [300, 400, 500, 700],
+			styles: ['normal', 'italic'],
 			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
 		},
 	],
 });
